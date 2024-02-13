a331da366066b45c014c6c1286d3f822888a8bae
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2024-02-13 13:50:01 +0100

    ok userView done

---
+++ mySchedule/FrontEnd/src/views/BillView.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { myUserStore } from '../services/PiniaServices';
+import { onBeforeMount, ref } from 'vue'
+import DateServices from '../services/DateServices';
+import PDFServices from '../services/PDFServices';
+
+const myUser=ref({});
+
+onBeforeMount(() => {
+    myUser.value=myUserStore().user;
+})
+
+function downloadBill(id){console.log(id);
+    PDFServices.downloadBill(id);
+}
+
+</script>
+
+<template>
+    <div class="logInForm">
+        Tus datos registrados: 
+        <hr>       
+        <p><span>Nombre:</span> {{ myUser.name }} {{ myUser.surname1 }} {{ myUser.surname2 }}</p>
+        <p><span>Nif:</span> {{ myUser.nif }}</p>
+        <p><span>Email:</span> {{ myUser.email }}</p>
+        <p><span>Tel&eacute;fono:</span> {{ myUser.phone }}</p>        
+    </div>
+
+    <div class="logInForm">
+        Tus facturas:
+        <hr>
+        <ul>
+            <li v-for="appo of myUser.appointmentsList" :key="appo">
+                <p>sesi&oacute;n del {{ DateServices.getDayFromDate(appo.appoDate)  }} {{ DateServices.changeFormatToDate(appo.appoDate) }}</p>
+                <p v-if="appo.hasBill===true"><img src="../assets/Images/pdf.png" alt="descargar factura" @click="downloadBill(appo.id)"></p>
+                <p v-else></p>
+            </li>
+        </ul>
+        
+    </div>
+
+</template>
+
+<style scoped>
+div{
+    width:50vw;
+}
+span{
+    font-size: 15px;
+    color: var(--color-text2);
+}
+hr{
+    width: 100%;
+    margin: 1vh auto;
+}
+p{
+    
+    margin-top:0.3vh;
+}
+ul{
+    list-style: none;
+    width: 100%;
+    margin: 0;
+    padding:0;
+}
+ul li{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px dashed var(--color-text);
+}
+img{
+    max-height: 4vh;
+}
+</style>
